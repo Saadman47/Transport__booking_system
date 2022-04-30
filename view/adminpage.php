9fd76ec4e2be27987a70e2a44746697246ef5102
Saadman47 <104510741+Saadman47@users.noreply.github.com>
2022-05-01 00:26:33 +0600

    First Commit

---
+++ view/adminpage.php
@@ -0,0 +1,38 @@
+<html>
+
+<head> 
+<link rel="stylesheet" type="text/css" href= "https://bootswatch.com/4/flatly/bootstrap.css">
+
+
+
+
+</head>
+<body style="background: url(https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80); background-size:100% 100%;">
+<center>
+<center>
+<font size = "10">Admin Page</font><br><br><br><br>
+
+<a class="btn btn-success" href ="modifybooking.php">Manage Bookings</a><br><br>
+
+<a class="btn btn-success" href="showbookinglist.php">Booking Page View</a><br><br>
+
+<a class="btn btn-success" href="bookedList.php">Booked List</a><br><br>
+
+<a class="btn btn-success" href="driver.php">Add Driver</a><br><br>
+
+<a class="btn btn-success" href="vehicle.php">Add Vehicle</a><br><br>
+
+<a class="btn btn-success" href= "showAdmin.php"> Show Admin</a><br><br>
+
+<a class="btn btn-success" href= "summary.php"> Summary </a><br><br>
+
+<script src = "https://bootswatch.com/_vendor/jquery/dist/jquery.min.js"> </script>
+
+<script src = "https://bootswatch.com/_vendor/popper.js/dist/umd/popper.min.js"> </script>
+
+<script src = "https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"> </script>
+
+
+</body>
+</center>
+</html>
---
+++ view/userpage.php
@@ -0,0 +1,25 @@
+<html>
+
+
+<head> 
+<link rel="stylesheet" type="text/css" href= "https://bootswatch.com/4/flatly/bootstrap.css">
+
+
+
+
+</head>
+<body style="background: url(https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80); background-size:100% 100%;">
+<center>
+<font size = "10">User Page</font><br><br><br><br>
+<a class="btn btn-success" href="showbookinglist.php">Available Bookings</a><br><br>
+
+<script src = "https://bootswatch.com/_vendor/jquery/dist/jquery.min.js"> </script>
+
+<script src = "https://bootswatch.com/_vendor/popper.js/dist/umd/popper.min.js"> </script>
+
+<script src = "https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"> </script>
+
+
+</body>
+</center>
+</html>
